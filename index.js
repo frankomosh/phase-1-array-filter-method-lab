974b3b7224317408636575c2dca6b9e2996cfe84
Frank-197 <frankomosh197@gmail.com>
2022-06-10 00:12:54 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(source, find) {
+    return source.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === find.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(source, testString) {
+    return source.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(source, findValue) {
+    return source.filter((record) => record.name === findValue);
+  } 
